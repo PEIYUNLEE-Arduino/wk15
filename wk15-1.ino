c4d1f4582cf46aebabc34b2c029732df47748a4b
PEIYUNLEE <39110605+PEIYUNLEE@users.noreply.github.com>
2018-06-06 11:11:06 +0800

    Add files via upload

---
+++ wk15-1.ino
@@ -0,0 +1,23 @@
+const int analogInPin = A0;
+//const int analogOutPin = 9;
+
+int sensorRead = 0;
+//int outputRead = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorRead = analogRead(analogInPin);
+  //outputRead = map(sensorRead,160,170,0,255);
+  //analogWrite(analogOutPin,outputRead);
+  Serial.print("sensor=");
+  Serial.print(sensorRead);
+//  Serial.print("sensor=");
+//  Serial.print(sensorRead);
+//  Serial.print("\t output=");
+//  Serial.print(outputRead);
+
+  delay(2);
+}
---
+++ wk15-2.ino
@@ -0,0 +1,24 @@
+const int analogInPin = A0;
+const int analogOutPin = 9;
+
+int sensorRead = 0;
+int outputRead = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorRead = analogRead(analogInPin);
+  if(sensorRead<60){sensorRead=60;}
+  else if(sensorRead>170){sensorRead=170;}
+  outputRead = map(sensorRead,60,170,0,255);
+  analogWrite(analogOutPin,outputRead);
+
+  Serial.print("sensor=");
+  Serial.print(sensorRead);
+  Serial.print("\t output=");
+  Serial.print(outputRead);
+
+  delay(2);
+}
